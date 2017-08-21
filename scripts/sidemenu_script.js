d1f1299ad8bcdb3f5339963b28fb1719c3af9a3a
Marcin Pabich <martinoqom@gmail.com>
2017-08-21 13:49:37 +0200

    Closing navbar when pressing outside

--- scripts/sidemenu_script.js
+++ scripts/sidemenu_script.js
@@ -3,8 +3,8 @@ var isOpened = false;
 
 function openNav() {
     if (!isOpened){
-        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-        document.getElementById("sideMenu").style.width = "250px";
+        //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        document.getElementById("sideMenu").style.width = "100%";
         $('#menuOpener').removeClass('fa-bars');
         $('#menuOpener').addClass('fa-times');
         isOpened = true;
@@ -17,7 +17,7 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("sideMenu").style.width = "0";
-    document.body.style.backgroundColor = "white";
+    //document.body.style.backgroundColor = "white";
     $('#menuOpener').removeClass('fa-times');
     $('#menuOpener').addClass('fa-bars');
     isOpened = false;
