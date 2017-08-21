1c4d844e933ee7491535d6861cfe030c34ba1c40
Marcin Pabich <martinoqom@gmail.com>
2017-08-21 13:55:11 +0200

    Fixed click problem

--- scripts/sidemenu_script.js
+++ scripts/sidemenu_script.js
@@ -3,7 +3,6 @@ var isOpened = false;
 
 function openNav() {
     if (!isOpened){
-        //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
         document.getElementById("sideMenu").style.width = "100%";
         $('#menuOpener').removeClass('fa-bars');
         $('#menuOpener').addClass('fa-times');
@@ -17,7 +16,6 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("sideMenu").style.width = "0";
-    //document.body.style.backgroundColor = "white";
     $('#menuOpener').removeClass('fa-times');
     $('#menuOpener').addClass('fa-bars');
     isOpened = false;
