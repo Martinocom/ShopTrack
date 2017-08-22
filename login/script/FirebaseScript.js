617c48820e9094d359665eb45f37bee943fff0db
nsantolini <nadirs1995@gmail.com>
2017-08-22 12:20:27 +0200

    Add Firebase JS ( Login, Register, Logout)

---
+++ login/script/FirebaseScript.js
@@ -0,0 +1,12 @@
+
+var config = {
+    apiKey: "AIzaSyD2Amywjsz8FWQ52tX6R-eJDbSXmt6bDvo",
+    authDomain: "shoptrack-658c5.firebaseapp.com",
+    databaseURL: "https://shoptrack-658c5.firebaseio.com",
+    projectId: "shoptrack-658c5",
+    storageBucket: "shoptrack-658c5.appspot.com",
+    messagingSenderId: "123504057587"
+    };
+
+firebase.initializeApp(config);
+console.log("DOPO INITIAZLICE");
